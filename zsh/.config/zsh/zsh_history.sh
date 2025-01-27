8b6f4478ba61f86dd104e3c2a7f5b20fd3ce6988
Emeraudia <stevan_metayer@orange.fr>
2025-01-27 23:27:18 +0100

    Add ignore command with space at start

--- zsh/.config/zsh/zsh_history.sh
+++ zsh/.config/zsh/zsh_history.sh
@@ -17,6 +17,15 @@ setopt APPEND_HISTORY
 # Do not enter command lines into the history list if they are duplicates of the previous event.
 setopt HIST_IGNORE_DUPS
 
+# Remove command lines from the history list when the first chatacter on the line is a space,
+# or when one of the expanded aliases contains a leading space.
+# Only normal aliases (not global or suffix aliases) have this behaviour.
+# Note that the command lingers in the internal history until the next command is entered before it vanishes,
+# allowing you to briefly reuse it or edit the line.
+# If you want to make it vanish right away without entering another command,
+# type a space and press return.
+setopt HIST_IGNORE_SPACE
+
 # When searching for history entries in the line editor,
 # do not display duplicates of a line previously found,
 # even if the duplicates are not contiguous.
